1d488106cfadaebb5972dc232134bce069a145f0
wahyu din <jangudin12345@gmail.com>
2025-09-25 09:25:52 +0700

    wahyudin 3.1

--- controllers/RLTigaTitikSatuController.js
+++ controllers/RLTigaTitikSatuController.js
@@ -3,35 +3,88 @@ import { get, show, rlTigaTitikDuaHeader, rlTigaTitikDuaDetail } from '../models
 import Joi from 'joi'
 import joiDate from "@joi/date"
 
+// export const getRLTigaTitikSatu = (req, res) => {
+//     const joi = Joi.extend(joiDate) 
+
+//     const schema = joi.object({
+//         provId: joi.string(),
+//         KabKotId: joi.string(),
+//         rsId: joi.string(),
+//         periode: joi.date().format("YYYY-M"),
+//         page: joi.number(),
+//         limit: joi.number()
+//     })
+
+//     const { error, value } =  schema.validate(req.query)
+
+//     if (error) {
+//         res.status(400).send({
+//             status: false,
+//             message: error.details[0].message
+//         })
+//         return
+//     }
+
+//     get(req, (err, results) => {
+//         const message = results.length ? 'data found' : 'data not found'
+//         res.status(200).send({
+//             status: true,
+//             message: message,
+//             data: results
+//         })
+//     })
+// }
+
 export const getRLTigaTitikSatu = (req, res) => {
-    const joi = Joi.extend(joiDate) 
+    const joi = Joi.extend(joiDate);
 
     const schema = joi.object({
-        rsId: joi.string().required(),
-        periode: joi.date().format("YYYY-M").required(),
+        provId: joi.string().allow(null, ''),       // boleh kosong dulu, nanti kita cek manual
+        KabKotId: joi.string().allow(null, ''),
+        rsId: joi.string().allow(null, ''),
+        periode: joi.date().format("YYYY-M").allow(null, ''),
         page: joi.number(),
         limit: joi.number()
-    })
+    });
 
-    const { error, value } =  schema.validate(req.query)
+    const { error, value } = schema.validate(req.query);
 
     if (error) {
-        res.status(400).send({
+        return res.status(400).send({
             status: false,
             message: error.details[0].message
-        })
-        return
+        });
     }
 
+    const { provId, KabKotId, rsId} = value;
+
+    // ✅ Validasi: kabkota atau provinsi wajib dipilih salah satu
+    if (!provId && !KabKotId && !rsId) {
+        return res.status(400).send({
+            status: false,
+            message: "Wajib pilih minimal salah satu: provId atau KabKotId atau rsId"
+        });
+    }
+
+    // ✅ Jika valid, lanjut ke model
     get(req, (err, results) => {
-        const message = results.length ? 'data found' : 'data not found'
+        if (err) {
+            return res.status(500).send({
+                status: false,
+                message: "Terjadi kesalahan pada server",
+                error: err.message
+            });
+        }
+
+        const message = results.length ? 'data found' : 'data not found';
         res.status(200).send({
             status: true,
             message: message,
             data: results
-        })
-    })
-}
+        });
+    });
+};
+
 
 export const showRLTigaTitikSatu = (req, res) => {
     show(req.params.id, (err, results) => {
