7fe39098f2349fdf121c59fa3a5722305257f4e4
Zullkifli <kifli997@gmail.com>
2026-07-28 10:06:37 +0700

    url

--- services/satusehat.service.js
+++ services/satusehat.service.js
@@ -1,15 +1,12 @@
 // services/satusehat.service.js
 import axios from "axios";
 
-const DEV_BASE_URL = process.env.DEV_SATUSEHAT_BASE_URL;
-const DEV_API_KEY = process.env.DEV_SATUSEHAT_API_KEY;
-
 const BASE_URL = process.env.SATUSEHAT_BASE_URL;
 const API_KEY = process.env.SATUSEHAT_API_KEY;
 
 export const fetchRL317FromSatuSehat = async (organization_id, year) => {
-  const res = await axios.get(`${DEV_BASE_URL}/rl317`, {
-    headers: { "X-API-Key": DEV_API_KEY },
+  const res = await axios.get(`${BASE_URL}/rl317`, {
+    headers: { "X-API-Key": API_KEY },
     params: { year, organization_id },
     timeout: 60000,
   });
@@ -18,8 +15,8 @@ export const fetchRL317FromSatuSehat = async (organization_id, year) => {
 };
 
 export const fetchRL318FromSatuSehat = async (organization_id, year) => {
-  const res = await axios.get(`${DEV_BASE_URL}/rl318`, {
-    headers: { "X-API-Key": DEV_API_KEY },
+  const res = await axios.get(`${BASE_URL}/rl318`, {
+    headers: { "X-API-Key": API_KEY },
     params: { year, organization_id },
     timeout: 60000,
   });
