9da898a2c4b2fb5d978ecb213a483c061b3106f3
Yohanessatria <yohanessatria29@gmail.com>
2026-07-28 15:51:39 +0700

    update punya zul

--- services/satusehat.service.js
+++ services/satusehat.service.js
@@ -4,6 +4,26 @@ import axios from "axios";
 const BASE_URL = process.env.SATUSEHAT_BASE_URL;
 const API_KEY = process.env.SATUSEHAT_API_KEY;
 
+export const fetchRL317FromSatuSehat = async (organization_id, year) => {
+  const res = await axios.get(`${BASE_URL}/rl317`, {
+    headers: { "X-API-Key": API_KEY },
+    params: { year, organization_id },
+    timeout: 60000,
+  });
+
+  return res.data;
+};
+
+export const fetchRL318FromSatuSehat = async (organization_id, year) => {
+  const res = await axios.get(`${BASE_URL}/rl318`, {
+    headers: { "X-API-Key": API_KEY },
+    params: { year, organization_id },
+    timeout: 60000,
+  });
+
+  return res.data;
+};
+
 export const fetchRL41FromSatuSehat = async (organization_id, periode) => {
   const res = await axios.get(`${BASE_URL}/rl41`, {
     headers: { "X-API-Key": API_KEY },
